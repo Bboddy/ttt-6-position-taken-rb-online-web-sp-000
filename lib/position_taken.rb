efb9d89c8b4a234f7eded9cae18a5eae901d52c6
Bryce B <bryce.boddy@gmail.com>
2021-01-15 01:15:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, pos)
   # return false if board[pos].nil? || board[pos] == "" || board[pos] == " "
-  false ? (board[pos] == "X" || board[pos] == "O") : true
+  (board[pos].nil? || board[pos] == "" || board[pos] == " ") ? false : true
 end
